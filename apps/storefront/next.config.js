5db5af65d171abff1c866e858cdf4d753e9f2b37
keaksey <keaksey.sum@gmail.com>
2023-08-08 20:54:52 +0700

    feat: add multiple tenants

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -44,7 +44,6 @@ module.exports = withBundleAnalyzer({
           },
         ],
       },
-
       {
         source: "/checkout/(.*)",
         headers: [{ key: "x-frame-options", value: "ALLOWALL" }],
